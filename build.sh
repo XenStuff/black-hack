c282b9ed7a35aff9f7860a2eaf3bcb774f600b61
wHo-EM-i <ehteshammalik18998@gmail.com>
2022-09-05 00:28:25 +0530

    ci: Lets build PA-13 topaz

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 # Normal build steps
 . build/envsetup.sh
-lunch nad_lavender-user
+lunch aospa_lavender-user
 
 # export variable here
 export TZ=Asia/Kolkata
 
 compile_plox () {
-make nad -j$(nproc --all)
+./rom-build.sh lavender -t user -j$(nproc --all)
 }
