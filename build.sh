974a31ae1eda9cd044a5188dd95c00beecda0799
wHo-EM-i <ehteshammalik18998@gmail.com>
2022-09-14 13:11:52 +0530

    ci: ArrowOS 12.1 4.19 | R
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 # Normal build steps
 . build/envsetup.sh
-lunch p404_lavender-userdebug
+lunch arrow_lavender-userdebug
 
 # export variable here
 export TZ=Asia/Kolkata
 export SELINUX_IGNORE_NEVERALLOWS=true
+export ARROW_GAPPS=true
 
 compile_plox () {
 make bacon -j$(nproc --all)
