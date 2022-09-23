MonsterHorny/MawaBruh

CI: Lets Make Nusantara A10 | R
Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

build.sh
@@ -1,10 +1,10 @@
 # Normal build steps
 . build/envsetup.sh
-lunch rom_lavender-userdebug
+lunch nad_lavender-userdebug
 
 # export variable here
 export TZ=Asia/Kolkata
 
 compile_plox () {
-make bacon -j$(nproc --all)
+make nad -j$(nproc --all)
 }

XenStuff/black-hack

build.sh
@@ -1,10 +1,10 @@
 # Normal build steps
 . build/envsetup.sh
-lunch rom_lavender-userdebug
+lunch nad_lavender-user
 
 # export variable here
 export TZ=Asia/Kolkata
 
 compile_plox () {
-make bacon -j$(nproc --all)
+make nad -j$(nproc --all)
 }